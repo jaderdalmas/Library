7677092d91707226063705e0a12263b8f8637f54
Jader Lasier Marchesin Dalmas <jader.dalmas@soulisto.com.br>
2019-02-22 20:46:00 -0300

    testes

---
+++ UnitTest/Repository.MockTest/SqlBookRepositoryMockTest.cs
@@ -0,0 +1,19 @@
+﻿using Model;
+using Repository;
+using System.Threading.Tasks;
+
+namespace UnitTest.Repository.MockTest
+{
+    public class SqlBookRepositoryMockTest : ISqlBookRepository
+    {
+        public Task<BookDto> GetBook(int Id)
+        {
+            return Task.FromResult(new BookDto());
+        }
+
+        public Task<long> PostBook(BookRequest book)
+        {
+            return Task.FromResult((long)1);
+        }
+    }
+}
---
+++ UnitTest/Service/BookServiceTest.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Model;
+using Service;
+using UnitTest.Repository.MockTest;
+
+namespace UnitTest.Service
+{
+    [TestClass]
+    public class BookServiceTest
+    {
+        public BookService GetService
+        {
+            get
+            {
+                return new BookService(new SqlBookRepositoryMockTest(), null);
+            }
+        }
+
+        [TestMethod]
+        public void GetBookById1()
+        {
+            var result = GetService.GetBookById(1).Result;
+
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void PostBook()
+        {
+            var book = new BookRequest()
+            {
+                Title = "Book title example",
+                Description = "Book description example",
+                ISBN = "9781617293290",
+                Language = "BR"
+            };
+
+            var result = GetService.PostBook(book).Result;
+
+            Assert.IsNotNull(result);
+        }
+    }
+}
