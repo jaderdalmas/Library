3a02cace09b9993f9de8dfc3480a6b8605399451
Jader Lasier Marchesin Dalmas <jader.dalmas@soulisto.com.br>
2019-08-16 11:50:21 -0300

    upgrade to 2.2

---
+++ Model/Extension/HttpRequestExtension.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+
+namespace Model.Extension
+{
+    public static class HttpRequestExtension
+    {
+        private static string GetUrl(this HttpRequest Request)
+        {
+            return $"{Request.Scheme}://{Request.Host}{Request.Path}";
+        }
+
+        public static Uri GetUri(this HttpRequest Request)
+        {
+            var referer = Request.Headers["Referer"].ToString();
+            return new Uri(string.IsNullOrWhiteSpace(referer) ? GetUrl(Request) : referer);
+        }
+    }
+}
